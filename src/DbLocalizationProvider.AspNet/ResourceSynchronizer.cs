8335db4a8f9c0cf6e699fe1cef919fbfc28240be
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2019-03-09 12:34:55 +0200

    fixed resource sync issue

--- src/DbLocalizationProvider.AspNet/ResourceSynchronizer.cs
+++ src/DbLocalizationProvider.AspNet/ResourceSynchronizer.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2017 Valdis Iljuconoks.
+﻿// Copyright (c) 2019 Valdis Iljuconoks.
 // Permission is hereby granted, free of charge, to any person
 // obtaining a copy of this software and associated documentation
 // files (the "Software"), to deal in the Software without
@@ -20,7 +20,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Globalization;
@@ -29,7 +28,6 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using DbLocalizationProvider.Cache;
-using DbLocalizationProvider.Commands;
 using DbLocalizationProvider.Internal;
 using DbLocalizationProvider.Queries;
 
