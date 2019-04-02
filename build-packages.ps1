43b232ffd3767b6c62bc54eff115cfd267186587
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2019-04-02 06:54:36 +0300

    added script for building JsResourceHandler

--- build-packages.ps1
+++ build-packages.ps1
@@ -3,5 +3,6 @@ cd .\.nuget
 .\nuget.exe pack ..\src\DbLocalizationProvider.AspNet\DbLocalizationProvider.AspNet.csproj -Properties Configuration=Release #-IncludeReferencedProjects
 .\nuget.exe pack ..\src\DbLocalizationProvider.AdminUI\DbLocalizationProvider.AdminUI.csproj -Properties Configuration=Release #-IncludeReferencedProjects
 .\nuget.exe pack ..\src\DbLocalizationProvider.Xliff\DbLocalizationProvider.Xliff.csproj -Properties Configuration=Release #-IncludeReferencedProjects
+.\nuget.exe pack ..\src\DbLocalizationProvider.JsResourceHandler\DbLocalizationProvider.JsResourceHandler.csproj -Properties Configuration=Release #-IncludeReferencedProjects
 
 cd ..\
