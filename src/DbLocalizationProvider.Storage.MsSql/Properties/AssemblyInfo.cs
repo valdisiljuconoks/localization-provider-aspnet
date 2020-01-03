e4f5a31c92e5c80b188d0fbb64a99898dc2b00f5
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2020-01-03 20:47:05 +0200

    Added MSSQL Storage implementation placeholder project

---
+++ src/DbLocalizationProvider.Storage.MsSql/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DbLocalizationProvider.AspNet")]
+[assembly: AssemblyDescription("LocalizationProvider Microsoft SQL Server Storage Implementation")]
+[assembly: Guid("bfff1d76-e64f-47be-b77e-e0873977b3b3")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyInformationalVersion("6.0.0")]
