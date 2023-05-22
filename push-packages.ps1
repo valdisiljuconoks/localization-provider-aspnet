47686086b9a658e092b5e9b1f86e57ba3d3ced08
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2023-05-22 15:56:39 +0300

    set version to 6.5.3

--- push-packages.ps1
+++ push-packages.ps1
@@ -1,7 +1,7 @@
 cd .\.nuget
 
-.\nuget.exe push .\LocalizationProvider.AspNet.6.5.2.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push .\LocalizationProvider.AdminUI.6.5.2.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push .\LocalizationProvider.Xliff.6.5.2.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push .\LocalizationProvider.AspNet.6.5.3.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push .\LocalizationProvider.AdminUI.6.5.3.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push .\LocalizationProvider.Xliff.6.5.3.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
